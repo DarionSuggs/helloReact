8c7fb26789f5e1f4f914b17ccb18575e98d75585
Darion Suggs <dariontsuggs@icloud.com>
2020-01-29 16:08:38 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Text(props) {
+  return <span style={{ color: "grey" }}>{props.value}</span>;
 }
-
+const App = () => {
+  return (
+    <h1>
+      Hello, <Text value="Darion" />
+    </h1>
+  );
+};
 export default App;
